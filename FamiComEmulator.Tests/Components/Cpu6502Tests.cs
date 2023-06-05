67573c0d66c21cc27305fac898c49f5d960b2d0f
carlos <carloscalvinp@gmail.com>
2023-06-05 02:04:05 +0200

    Add ADC instruction test

--- FamiComEmulator.Tests/Components/Cpu6502Tests.cs
+++ FamiComEmulator.Tests/Components/Cpu6502Tests.cs
@@ -21,8 +21,8 @@ namespace FamiComEmulator.Tests.Components
             //Arrange
             _bus.Write(0xFFFC, 0x00);
             _bus.Write(0xFFFD, 0x20);
-            _bus.Write(0x2000, 0xA9);
-            _bus.Write(0x2001, 0x42);
+            _bus.Write(0x2000, 0xA9); // LDA immediate
+            _bus.Write(0x2001, 0x42); // Value to load into Accumulator
 
             _bus.Cpu.Reset();
             while (!_bus.Cpu.Finish())
@@ -40,6 +40,43 @@ namespace FamiComEmulator.Tests.Components
             Assert.Equal(0x42, _bus.Cpu.Accumulator);
             Assert.True(_bus.Cpu.GetFlag(Flags6502.Zero) == 0);
             Assert.False(_bus.Cpu.GetFlag(Flags6502.Negative) == 1);
+        }
+
+        [Fact]
+        public void TestADCInstructionExecution()
+        {
+            // Arrange
+            _bus.Write(0xFFFC, 0x00);
+            _bus.Write(0xFFFD, 0x20);
+            _bus.Write(0x2000, 0xA9); // LDA immediate
+            _bus.Write(0x2001, 0x10); // Value to load into Accumulator
+
+            _bus.Write(0x2002, 0x69); // ADC immediate
+            _bus.Write(0x2003, 0x05); // Value to add to Accumulator
+
+            _bus.Cpu.Reset();
+            while (!_bus.Cpu.Finish())
+            {
+                _bus.Cpu.Clock();
+            }
+
+            // Act
+            for (int i = 0; i < 4; i++)
+            {
+                _bus.Cpu.Clock();
+            }
+
+            // Assert
+            int carryFlag = _bus.Cpu.GetFlag(Flags6502.CarryBit);
+            int zeroFlag = _bus.Cpu.GetFlag(Flags6502.Zero);
+            int overflowFlag = _bus.Cpu.GetFlag(Flags6502.Overflow);
+            int negativeFlag = _bus.Cpu.GetFlag(Flags6502.Negative);
+
+            Assert.Equal(0x15, _bus.Cpu.Accumulator);
+            Assert.Equal(0, carryFlag);
+            Assert.Equal(0, zeroFlag);
+            Assert.Equal(0, overflowFlag);
+            Assert.Equal(0, negativeFlag);
         }
     }
 }
